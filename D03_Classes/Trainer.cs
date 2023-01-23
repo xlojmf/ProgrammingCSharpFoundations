0c35b9a1db3f5682cdb010b063eda57c28c6f1e8
Jose Ferraz <me@joseferraz.pt>
2023-01-23 22:33:22 +0000

    D04 Data Conversion

---
+++ D03_Classes/Trainer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace D03_Classes
+{
+    internal class Trainer
+    {
+        #region Fields(atributes): privados; variaveis internas da classe e que suportam as propriedades classicas
+        private int trainerId;
+        private string trainerName;
+        private bool ccp;
+
+        #endregion
+
+
+        #region Properties (publicas e referem as caracterias do objecto)
+        // Propriedade Clássica (propfull)
+        public int TrainerId {        
+            get { return trainerId; } 
+            set { trainerId = value; }        
+        }
+        public string TrainerName { 
+            get { return trainerName; }
+            set { trainerName = value; }
+        }
+
+        //Auto Implemented (Propriedade Automatica): criada com snipet (prop)
+        public string Email { get; set; }
+
+        // Bodied Expression: usa o field e um operador(=>)
+        public bool Ccp
+        {
+            get => ccp;
+            set => ccp = value;
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Trainer()
+        {
+            TrainerId = 0;
+            TrainerName= string.Empty;
+            Email = string.Empty;
+            Ccp = false;
+        }
+
+        public Trainer(int trainerid, string trainername, string email, bool ccp)
+        {
+            TrainerId= trainerid;
+            TrainerName= trainername;
+            Email= email;
+            Ccp= ccp;
+        }
+
+        #endregion
+
+        #region Methods
+        internal void Create()
+        {
+            Console.WriteLine("Id:");
+            TrainerId = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Name: ");
+            TrainerName = Console.ReadLine();
+            Console.WriteLine("Name: ");
+            Email = Console.ReadLine();
+            Console.WriteLine("Start Date: ");
+            Ccp = Convert.ToBoolean(Console.ReadLine());
+
+        }
+
+        internal void List()
+        {
+            Console.WriteLine("Course ID\tName\tStart Date\tEnd Date\tDuration");
+            Console.WriteLine($"\t{TrainerId}\t{TrainerName}\t{Email}\t{Ccp}\n");
+        }
+
+        #endregion
+
+    }
+
+
+}
---
+++ D04_DataConversion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D04_DataConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variaveis
+            string string01 = "1";
+            int int01 = 10;
+            int int02 = 20;
+            long long01 = 2000000;
+            float float01 = 3.8F;
+            double double01 = 10.5;
+            #endregion
+
+            #region Sem Conversão
+            Console.WriteLine(string01 + int01);
+            #endregion
+
+            #region Conversão Implicita
+            Console.WriteLine(int01 + long01); //numericos sao compativeis, e se são compativeis tem conversao implicita
+            Console.WriteLine(float01 + double01);
+            #endregion
+
+            #region Conversao Explicita
+            // Método ToString(): data type extension method --> converter para string
+            // Classe Convert: converter diferentes tipos de dados
+            // Método Parse(): converter string em número
+            // Método TryParse(): converter string em número
+            // Operator Cast: converter números em outros números (edited)
+            #endregion
+
+            #region
+            #endregion
+
+            Console.ReadKey();
+            
+        }
+    }
+}
